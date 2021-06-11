03fa7be73201d5033e168f0d908e5b90fbd9643b
U-DESKTOP-DOLL7G1\tim-k <tim-kaiser@gmx.de>
2021-06-12 01:30:44 +0200

    few attractors done

---
+++ aizawa.js
@@ -0,0 +1,129 @@
+let x;
+let y;
+let z;
+let dt;
+
+let a;
+let b;
+let c;
+let d;
+let e;
+let f;
+
+let prevX;
+let prevY;
+let prevZ;
+
+let ptList;
+let pt;
+function setup(){
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    background(0);
+    frameRate(30);
+    colorMode(HSL);
+    prevX = 0.1;
+    prevY = 0;
+    prevZ = 0;
+    x = 0.1;
+    y = 0;
+    z = 0;
+    dt = 0.01;
+    a = 0.95;
+    b = 0.7;
+    c = 0.6;
+    d = 3.5;
+    e = 0.25;
+    f = 0.1;
+    let dx = 0.1;
+    ptList = new Array();
+    for(let i = 0; i < 15; i++){
+        pt = new Pt(x + dx,y,z);
+        pt.setup();
+        ptList.push(pt);
+        dx = dx + 0.1;
+    }
+}
+
+function draw(){
+    background(0);
+      rotateX(radians(90));
+      rotateY(radians(45));
+      translate(200,-400);
+
+     scale(300);
+    // let dx = (z-b)*x - d*y;
+    // let dy = d*x + (z-b)*y;
+    // let dz = c + a*z - z*z*z/3 - (x*x + y*y)*(1 + e*z) + f*z*x*x*x;
+    
+    // x = (x + dx * dt);
+    // y = (y + dy * dt);
+    // z = (z + dz * dt);
+    // stroke(color(52, 100, 44));
+    // line(prevX, prevY, prevZ, x,y,z);
+
+    // prevX = x;
+    // prevY = y;
+    // prevZ = z;
+    for(const pt of ptList){
+        pt.move();
+        pt.draw();
+    }
+}
+
+class Pt{
+    constructor(x,y,z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.points = new Array();
+        this.col1 = color(52, 100, 44, 1);
+        this.col2 = color(25, 100, 36, 0.7);
+        this.col3 = color(53, 100, 72);
+        this.col4 = color(39, 100, 33, 0.8);
+    }
+    setup(){
+        this.points.push([this.x, this.y, this.z]);
+    }
+
+    getX(){
+        return this.x;
+    }
+    getY(){
+        return this.y;
+    }
+    getZ(){
+        return this.z;
+    }
+    move(){
+        let dx = (this.z-b)*this.x - d*this.y;
+        let dy = d*this.x + (this.z-b)*this.y;
+        let dz = c + a*this.z - this.z*this.z*this.z/3 - (this.x*this.x + this.y*this.y)*(1 + e*this.z) + f*this.z*this.x*this.x*this.x;
+        
+        this.x = (this.x + dx * dt);
+        this.y = (this.y + dy * dt);
+        this.z = (this.z + dz * dt);
+        this.points.push([this.x, this.y, this.z]);
+        if(this.points.length > 250){
+            this.points.shift();
+        }
+    }
+    draw(){
+        strokeWeight(1);
+        for(let i = 1; i < this.points.length; i++){
+            stroke(this.col1);
+            if(this.points[i][1] > 1){
+                stroke(this.col3);
+            }else if(this.points[i][1] < -0.1){
+                stroke(this.col2);
+            }else if(this.points[i][1] > -0.1 && this.points[i][1] < 0.2){
+                stroke(this.col4);
+            }
+            line(this.points[i-1][0],this.points[i-1][1],this.points[i-1][2], this.points[i][0],this.points[i][1],this.points[i][2]);
+            //point(this.points[i].x,this.points[i].y,this.points[i].z);
+        }
+    }
+    setColor(col){
+        this.col = col;
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,348 @@
+let mousePositions = Array([20,20]);
+let counter;
+let lastMousePosX = 20;
+let lastMousePosY = 20;
+let font;
+
+let showText = 1;
+let showCursor = 1;
+let attractorMode = 1;
+let backgroundCol = 0;
+
+let attractor;
+//TODO: new class for display function; dot, line, and structure variants
+
+function preload() {
+  font = loadFont('assets/VCR_OSD_MONO.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(backgroundCol);
+  frameRate(60);
+  colorMode(RGB);
+
+
+  if(attractorMode == 1){
+    attractor = new LorenzAttractor(5, 0.01);
+  }else if(attractorMode == 2){
+    attractor = new RoesslerAttractor(30, 0.01);
+  }else if(attractorMode == 3){
+    attractor = new AizawaAttractor(1, 0.01);
+  }else if(attractorMode == 4){
+    attractor = new ChenLeeAttractor(10, 0.01);
+  }
+  if(attractorMode != 0){
+    attractor.setup();
+  }
+
+  counter = 0; 
+}
+
+function draw() {
+
+  if(showText == 1){
+    //stroke('white');
+    fill('white');
+    textFont(font);
+    textSize(15);
+    text('c to show/hide cursor',-800,-200);
+    text('t to show/hide help text', -800, -185);
+    text('1-4 to cycle through attractors', -800, -170);
+  }
+
+  //need this for correct tracking in WEBGL space
+  //IDK fix mouse tracker, larger ellipse needs to lag behind small circle, but catch up if theres no mouse movement
+  noStroke();
+
+  fill(backgroundCol);
+  ellipse(mousePositions[mousePositions.length -1][0],mousePositions[mousePositions.length -1][1], 20, 20);
+
+  if(showCursor == 1){
+    let mpX = mouseX - width/2;
+    let mpY = mouseY - height/2;
+
+    lastMousePosX = mpX;
+    lastMousePosY = mpY;
+
+    fill(255, 255, 255, 100);
+    ellipse(lastMousePosX, lastMousePosY, 20, 20);
+
+    mousePositions.push([mpX,mpY]);
+    fill('white');
+    ellipse(mpX, mpY, 4, 4);
+  }
+  
+  if(attractor instanceof AizawaAttractor){
+    //position the attractor correctly in 3D space
+    rotateX(radians(90));
+    rotateY(radians(45));
+    translate(200,-400);
+
+    scale(400);
+    attractor.move();
+    attractor.display();
+  }else if(attractor instanceof LorenzAttractor){
+    scale(10);
+    attractor.move();
+    attractor.display();
+  }else if(attractor instanceof RoesslerAttractor){
+    translate(-100,0);
+    rotateY(radians(45));
+    rotateX(radians(20));
+    scale(50);
+    attractor.move();
+    attractor.display();
+  }else if(attractor instanceof ChenLeeAttractor){
+    //not optimal yet
+    scale(10);
+    attractor.move();
+    attractor.display();
+  }
+  // counter++;
+}
+function hideText(){
+  noStroke();
+  fill(backgroundCol);
+  rect(-80, -26, 30, 10);
+}
+
+function keyPressed(){
+  //c
+    
+  if (keyCode === 67){
+    showCursor *= -1;
+  }
+  if(keyCode === 84){
+    showText *= -1;
+    hideText();
+  }
+  //1
+  if(keyCode === 49){
+    attractorMode = 1;
+    setup();
+  }else if(keyCode === 50){
+    attractorMode = 2;
+    setup();
+  }else if(keyCode === 51){
+    attractorMode = 3;
+    setup();
+  }else if(keyCode === 52){
+    attractorMode = 4;
+    setup();
+  }
+
+
+}
+
+
+class ChenLeeAttractor{
+  constructor(points, xIncr){
+    this.pointList = new Array();
+    this.points = points;
+    this.increment = xIncr;
+  }
+  setup(){
+    let xPos = 0.1
+    for(let i = 0; i < this.points; i++){
+      let pt = new Point(xPos,0,4.5, 5, -10, -0.38, null, null, null);
+      this.pointList.push(pt);
+      xPos += this.increment;
+    }
+  }
+
+  move(){
+    for(const point of this.pointList){
+      point.move('chenlee');
+    }
+  }
+  display(){
+    for(const point of this.pointList){
+      point.display();
+    }
+  }
+
+}
+
+class AizawaAttractor{
+  constructor(points, xIncr){
+    this.pointList = new Array();
+    this.points = points;
+    this.increment = xIncr;
+  }
+  setup(){
+    let xPos = 0.1
+    for(let i = 0; i < this.points; i++){
+      let pt = new Point(xPos,0,0, 0.95, 0.7, 0.6, 3.5, 0.25, 0.1);
+      this.pointList.push(pt);
+      xPos += this.increment;
+    }
+  }
+
+  move(){
+    for(const point of this.pointList){
+      point.move('aizawa');
+    }
+  }
+  display(){
+    for(const point of this.pointList){
+      point.display();
+    }
+  }
+}
+
+
+class RoesslerAttractor{
+  constructor(points, xIncr){
+    this.points = points;
+    this.increment = xIncr;
+    this.pointList = new Array();
+  }
+
+  setup(){
+    let xPos = 0.1;
+    for(let i = 0; i < this.points; i++){
+      let pt = new Point(xPos, 0, 0, 0.2, 0.2, 5.7, null, null, null);
+      this.pointList.push(pt);
+      xPos += this.increment;
+    }
+  }
+
+  move(){
+    for(const point of this.pointList){
+      point.move('roessler');
+    }
+  }
+
+  display(){
+    for(const point of this.pointList){
+      point.display();
+    }
+  }
+}
+
+class LorenzAttractor{
+  constructor(points, xIncr){
+    this.points = points;
+    this.pointList = new Array();
+    this.increment = xIncr;
+
+  }
+
+  setup(){
+    let xPos = 0.1;
+    for(let i = 0; i < this.points; i++){
+      let pt = new Point(xPos, 0, 0, 10, 28, 8.0/3.0, null, null, null);
+      this.pointList.push(pt);
+      xPos += this.increment;
+
+    }
+  }
+
+  move(){
+    for(const point of this.pointList){
+      point.move('lorenz');
+    }
+  }
+
+  display(){
+    for(const point of this.pointList){
+      point.display();
+
+    }
+  }
+}
+
+//constructs a single point for a chaos attractor
+class Point {
+    constructor(x,y,z, a,b,c,d,e,f){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+
+        this.col = color(random()*255, random()*255, random()*255);
+        //this.col = color(random()*255, random()*50, random()*50);
+
+    }
+
+    move(mode){
+      //moving according to the lorenz attractor calculations
+      if(mode == 'lorenz'){
+          let a = this.a;
+          let b = this.b;
+          let c = this.c;
+      
+          let dt = 0.01;
+          let dx = (a * (this.y - this.x)) * dt;
+          let dy = (this.x * (b - this.z) - this.y) * dt;
+          let dz = (this.x * this.y - c * this.z) * dt;
+
+          this.x = this.x + dx;
+          this.y = this.y + dy;
+          this.z = this.z + dz;
+      //moving according to the roessler attractor calculations
+      }else if(mode == 'roessler'){
+        let a = this.a;
+        let b = this.b;
+        let c = this.c;
+    
+        let dt = 0.01;
+        let dx = (- this.y - this.z) * dt;
+        let dy = (this.x + a * this.y) * dt;
+        let dz = (b + this.z * (this.x - c)) * dt;
+        
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+        this.z = this.z + dz;
+      }else if(mode == 'aizawa'){
+        let a = this.a;
+        let b = this.b;
+        let c = this.c;
+        let d = this.d;
+        let e = this.e;
+        let f = this.f;
+
+        let x = this.x;
+        let y = this.y;
+        let z = this.z;
+
+        let dx = (z-b)*x - d*y;
+        let dy = d*x + (z-b)*y;
+        let dz = c + a*z - z*z*z/3 - (x*x + y*y)*(1 + e*z) + f*z*x*x*x;
+    
+        let dt = 0.01;        
+        this.x = (this.x + dx * dt);
+        this.y = (this.y + dy * dt);
+        this.z = (this.z + dz * dt);
+      }else if(mode == 'chenlee'){
+        let a = this.a;
+        let b = this.b;
+        let c = this.c;
+
+        let x = this.x;
+        let y = this.y;
+        let z = this.z;
+
+        let dt = 0.004;
+
+        let dx = a * x - y * z;
+        let dy = b * y + x * z;
+        let dz = c * z + x * (y/3);
+
+        this.x = this.x + dx * dt;
+        this.y = this.y + dy * dt;
+        this.z = this.z + dz * dt;
+      }
+    }
+
+    display(){
+        stroke(this.col);
+        point(this.x, this.y, this.z);
+    }
+}
